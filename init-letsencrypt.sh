90907cb870a5fe81d6f2295fdc7cab6235135379
abevince <vincentabesamis03@gmail.com>
2025-02-22 18:21:11 +0800

    Add domain details

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Replace with your domain
-domains=(your_domain.com)
+domains=(remudi.com)
 rsa_key_size=4096
 data_path="./certbot"
-email="" # Adding a valid address is strongly recommended
+email="vincentabesamis03@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
